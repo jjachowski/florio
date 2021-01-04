0fdc89b22ae97bd70b673ad6212c6bc04bee35b2
Jakub Jachowski <kubajachowski@gmail.com>
2021-01-04 22:09:09 +0100

    added icons to plant page, change quote style to single which may cause bigger commits

--- web/src/pages/plant/[id].tsx
+++ web/src/pages/plant/[id].tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Heading, Image } from '@chakra-ui/react';
-import { Head } from 'next/document';
+import { MoonIcon, SunIcon, TimeIcon } from '@chakra-ui/icons';
+import { Box, Flex, Heading, HStack, Image } from '@chakra-ui/react';
 import React from 'react';
 import { Layout } from '../../components/Layout';
 import { Navbar } from '../../components/Navbar';
@@ -13,22 +13,34 @@ const Plant: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Layout variant="full-screen" p={20}>
-        <Flex direction="row">
+      <Layout variant='full-screen' p={20}>
+        <Flex direction='row'>
           <Image
-            shadow="2xl"
+            shadow='2xl'
             rounded={20}
-            objectFit="cover"
-            w="30%"
-            src="https://zielony-parapet.pl/5136-thickbox_default/calathea-lancifolia-kalatea-lancetolistna.jpg"
-            alt="Kalatea"
+            objectFit='cover'
+            w='30%'
+            src='https://zielony-parapet.pl/5136-thickbox_default/calathea-lancifolia-kalatea-lancetolistna.jpg'
+            alt='Kalatea'
             mr={20}
           />
-          <Flex w="100%" p={10} direction="column" shadow="2xl" rounded={20}>
-            <Heading>
-              {data?.plant?.names.find((n) => n.isPrimary)?.name}
-            </Heading>
-            <Box>{data?.plant && <PlantOtherNames plant={data?.plant} />}</Box>
+          <Flex w='100%' p={10} direction='column' shadow='2xl' rounded={20}>
+            <Flex direction='row'>
+              <Flex direction='column'>
+                <Heading>
+                  {data?.plant?.names.find((n) => n.isPrimary)?.name}
+                </Heading>
+                <Box>
+                  {data?.plant && <PlantOtherNames plant={data?.plant} />}
+                </Box>
+              </Flex>
+              <HStack mb='auto' spacing={2} ml='auto' fontSize='3rem'>
+                <SunIcon />
+                <MoonIcon />
+                <TimeIcon />
+              </HStack>
+            </Flex>
+
             <Box>{data?.plant?.description}</Box>
           </Flex>
         </Flex>
