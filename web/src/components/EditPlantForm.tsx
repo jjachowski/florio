289e11732ed71ac281ef36c2f2159b1ec9feaaa2
Jakub Jachowski <kubajachowski@gmail.com>
2021-03-11 21:57:12 +0100

    working on edit plant form

--- web/src/components/EditPlantForm.tsx
+++ web/src/components/EditPlantForm.tsx
@@ -1,24 +1,35 @@
-import { Box, Button, VStack } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, VStack } from '@chakra-ui/react';
 import { Form, Formik, FormikErrors } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import { FullPlantFragment } from '../generated/graphql';
+import { FileField } from './FileField';
 import { FormField } from './FormField';
+import {
+  Image as CloudinaryImage,
+  Video,
+  Transformation,
+  CloudinaryContext,
+} from 'cloudinary-react';
 
 interface EditPlantFormProps {
-  plantToEdit: FullPlantFragment
+  plantToEdit: FullPlantFragment;
 }
 
 export const EditPlantForm: React.FC<EditPlantFormProps> = ({
-  plantToEdit
+  plantToEdit,
 }) => {
   const {} = plantToEdit;
+  const hiddenFileInput = React.useRef<HTMLInputElement>(null);
+  const [filePreviews, setFilePreviews] = useState<
+    { name: string; size: number }[]
+  >([]);
+
   return (
     <Formik
       initialValues={{
         primaryName: '',
         otherNames: '',
         description: '',
-        
       }}
       onSubmit={async (values, { setErrors }) => {
         // const { primaryName, description } = values;
@@ -50,6 +61,47 @@ export const EditPlantForm: React.FC<EditPlantFormProps> = ({
                 isRequired
                 placeholder='Odmiana Fascinator Tricolor jest...'
                 label='Opis'
+              />
+              <HStack spacing={4} width='100%'>
+                {plantToEdit.images.map((image) => (
+                  <Box
+                    key={image}
+                    borderWidth='medium'
+                    borderColor='gray.200'
+                    borderStyle='dotted'
+                    _hover={{ borderColor: 'red.400' }}
+                    rounded={10}
+                    overflow='hidden'
+                  >
+                    <CloudinaryImage cloudName='disxisevt' publicId={image}>
+                      <Transformation
+                        height='100'
+                        quality='40'
+                        width='100'
+                        crop='fill'
+                      />
+                    </CloudinaryImage>
+                  </Box>
+                ))}
+              </HStack>
+
+              <FileField
+                name='images'
+                isRequired
+                hiddenFileInput={hiddenFileInput}
+                filePreviews={filePreviews}
+                onAccepted={(files) => {
+                  if (!files) {
+                    return;
+                  }
+                  setFilePreviews(
+                    Array.from(files).map((f) => {
+                      return { name: f.name, size: f.size };
+                    })
+                  );
+
+                  props.setFieldValue('images', files);
+                }}
               />
             </VStack>
 
